4dc8d1e5c2c7d8d83b927e53ef0b993e61e3b0d6
Benjamin Huang <benjaminhuanghuang@gmail.com>
2022-02-08 13:16:18 -0800

    update

---
+++ 02-signal-slot/README.md
@@ -0,0 +1,7 @@
+
+
+Widgets send signals, like click
+
+```
+  result_btn = QPushButton('Entger', clicked=lambda: self.result_field.setText('Test'))
+```
--- Demos/calculator.py
+++ Demos/calculator.py
@@ -1,6 +1,3 @@
-import imp
-
-
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QPushButton, QLineEdit, QStyleFactory
 
@@ -16,6 +13,10 @@ class MainWindow(QWidget):
     self.setLayout(QVBoxLayout())
     self.keypad()
 
+    self.temp_nums =[]
+    self.fin_nums= []
+
+
     self.show()
 
   def keypad(self):
@@ -23,27 +24,25 @@ class MainWindow(QWidget):
     container.setLayout(QGridLayout())
 
     #
-    result_field = QLineEdit()
-    btn_result = QPushButton('Enter')
-    btn_clear = QPushButton('Clear')
-    btn_9 = QPushButton('9')
-    btn_8 = QPushButton('8')
-    btn_7 = QPushButton('7')
-    btn_6 = QPushButton('6')
-    btn_5 = QPushButton('5')
-    btn_4 = QPushButton('4')
-    btn_3 = QPushButton('3')
-    btn_2 = QPushButton('2')
-    btn_1 = QPushButton('1')
-    btn_0 = QPushButton('0')
-    btn_9 = QPushButton('Enter')
-    btn_9 = QPushButton('Enter')
-    btn_plus = QPushButton('+')
-    btn_mins = QPushButton('-')
-    btn_mult = QPushButton('*')
-    btn_divd = QPushButton('/')
+    self.result_field = QLineEdit()
+    btn_result = QPushButton('Enter', clicked=self.func_result)
+    btn_clear = QPushButton('Clear', clicked=self.clear_calc)
+    btn_9 = QPushButton('9', clicked= lambda: self.num_press('9'))
+    btn_8 = QPushButton('8', clicked= lambda: self.num_press('8'))
+    btn_7 = QPushButton('7', clicked= lambda: self.num_press('7'))
+    btn_6 = QPushButton('6', clicked= lambda: self.num_press('6'))
+    btn_5 = QPushButton('5', clicked= lambda: self.num_press('5'))
+    btn_4 = QPushButton('4', clicked= lambda: self.num_press('4'))
+    btn_3 = QPushButton('3', clicked= lambda: self.num_press('3'))
+    btn_2 = QPushButton('2', clicked= lambda: self.num_press('2'))
+    btn_1 = QPushButton('1', clicked= lambda: self.num_press('1'))
+    btn_0 = QPushButton('0', clicked= lambda: self.num_press('0'))
+    btn_plus = QPushButton('+', clicked= lambda: self.func_press('+'))
+    btn_mins = QPushButton('-', clicked= lambda: self.func_press('-'))
+    btn_mult = QPushButton('*', clicked= lambda: self.func_press('*'))
+    btn_divd = QPushButton('/', clicked= lambda: self.func_press('/'))
     # put into grid
-    container.layout().addWidget(result_field, 0,0,1,4)
+    container.layout().addWidget(self.result_field, 0,0,1,4)
     container.layout().addWidget(btn_result, 1,0,1,2)
     container.layout().addWidget(btn_clear, 1,2,1,2)
 
@@ -66,6 +65,33 @@ class MainWindow(QWidget):
     container.layout().addWidget(btn_divd, 5,3)
 
     self.layout().addWidget(container)
+
+  def num_press(self, key_number):
+    self.temp_nums.append(key_number)
+    temp_string = ''.join(self.temp_nums)
+    if self.fin_nums:
+      self.result_field.setText(''.join(self.fin_nums)+ temp_string)
+    else:
+      self.result_field.setText(temp_string)
+
+  def func_press(self, operator):
+    temp_string = ''.join(self.temp_nums)
+    self.fin_nums.append(temp_string)
+    self.fin_nums.append(operator)
+    self.temp_nums =[]
+    self.result_field.setText(''.join(self.fin_nums))
+
+  def func_result(self):
+    fin_string = ''.join(self.fin_nums) +''.join(self.temp_nums)
+    result_string = eval(fin_string)
+    fin_string += '='
+    fin_string += str(result_string)
+    self.result_field.setText(fin_string)
+
+  def clear_calc(self):
+    self.result_field.clear()
+    self.temp_nums=[]
+    self.fin_nums=[]
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
